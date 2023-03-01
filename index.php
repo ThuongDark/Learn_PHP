6e2a96356f6c491ac9062eab0ec6df081e576078
ThuongDark <leoskillvip@gmail.com>
2023-03-01 11:12:57 +0700

    Bài 3: Number và var_dump

--- index.php
+++ index.php
@@ -1,48 +1,66 @@
 <?php
-//Đếm dộ dài của chuỗi bao gồm dấu cách [space]
-echo "Đếm các từ trong chuỗi này - 'Hello test' =>" . strlen('Hello test') . "</br>";
 
 
-// Trả về số chuỗi nếu string là số sẽ bỏ qua
-echo str_word_count('Hello word fddd d') . "</br>"; //4
+/*
+    var_dump() sẽ in ra thông tin của biến gồm kiểu dữ liệu của biến và giá trị.
+    is_* :* => int, bool, float, nummeric, string, array, object
+    int: là số nguyên
+    float: là số thập phân
+    bool: trả về true hoặc false
+    nummeric: kiểm tra có phải là số hay không nếu string trả về false;
+    NaN là viết tắt của Not a Number (không phải là số)
+    ...
 
-//Đảo ngược chuỗi;
-echo strrev('Hello world') . "</br>";
-
-//----------------Tìm từ trong chuỗi----------------------
-/* Syntax : strpos("str","char","position")
-str: chuỗi;
-char: ký tự muốn tìm kiếm
-position: vị trí bắt đầu tìm kiếm if rỗng mặc định bắt đầu position 0
-
-Note: nếu không có ký tự tồn tại trong chuỗi thì sẽ trả về fasle
-*/
-echo strpos("Hello word? w  w w ", "w") . "</br>";
-
-// $str = "Hello word";
-// $char = "word";
-// $strpos = strpos($str,$char);
-
-// if ($strpos == false)
-// {
-//   echo "không tìm thấy ký tự ".$char;
-// }
-// else
-// {
-// echo "Đã tìm thấy ký tự ".$char." tại vị trí ".$strpos;
-// }
-
-
-
-//-------------- Thay đổi ký tự---------------
-/* Syntax : str_replace("strSearch","StrReplace","subject")
-
-strSearch : từ muốn tìm kiếm;
-strReplace : từ muốn thay đổi
-subbject(chủ thể) : là mảng muốn thay đổi
 
 */
-echo str_replace("world","Dolly","Hello mọi người nha world");
+// =============int là số nguyên là số không có dấu phẩy=================================
+$n_int = 6868;
+var_dump(is_int($n_int));
+echo "</br>"."============"."</br>";
 
+
+
+// ==============float là số có đấy chấm và có thể lưu trữ số lớn hơn int================
+$n_float =1.9e411;//Float max
+var_dump($n_float);
+var_dump(is_float($n_float));
+echo "</br>"."============"."</br>";
+
+
+/*=================== NaN là viết tắt của Not a Number (không phải là số)================== */
+
+$nan = acos(8);
+// var_dump($nan);
+// echo var_dump(is_nan($nan)); ==> result :True
+
+echo "</br>"."============"."</br>";
+
+/*============== kiểm tra dữ liệu đó có phải là số hay không? nếu string -> fasle================ */
+$n_nummeric = 6655;//true
+$n_nummeric = "222";//true
+$n_nummeric = "test";//fasle
+var_dump(is_numeric($n_nummeric));
+
+echo "</br>"."============"."</br>";
+
+/*=========================PHP Casting String and Float to Integers =======================
+**************************(Truyền chuỗi và thập phân thành số nguyên)*********************** 
+
+Syntax: (_)$...
+Mô tả : _ là kiểu dữ liệu muốn chuyển đổi string,float,int,object,array.
+        $... là tên biến muốn chuyển đổi.
+Note: có thể sử dụng var_dump để xem trực quan.
+*/
+
+$xy = 2343.99;
+var_dump($xy);
+echo " ==> ";
+$cast = (array)$xy;
+var_dump($cast);
+
+echo "</br>"."============"."</br>";
+/* Kiểm tra nó có phải lá null không ? */
+$n_null=null;
+echo var_dump(is_null($n_null));
 
 ?>
