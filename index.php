20ce1ad86fb36fe867f399051703fdf0db7bcd82
ThuongDark <leoskillvip@gmail.com>
2023-03-08 11:12:19 +0700

    Bài 7.1: Vòng lặp for()

--- index.php
+++ index.php
@@ -1,18 +1,8 @@
 <?php
-
-// Xem vòng lặp hoạt động
-$number =0;
-$condition =2;
-do{
-echo "Giá trị của number: ".$number;
-$number++;
-echo "- Giá trị đã tăng: ".$number;
-echo "</br>";
-//Thông báo điều kiện thoả mãn
-if ($number==$condition)
-{
-    echo "Giá trị của hai biến đã thoả mãn"."</br>";
-}elseif ($number>$condition)
-{echo "Giá trị của biến number đã tăng "."</br>";}
-}while($number<=$condition)
+/*for (init counter; test counter; increment counter) {
+  code to be executed for each iteration;
+} */
+for ($number = 0; $number <= 100; $number+=10) {
+    echo "Giá trị của number sẽ tăng: " . $number . "</br>";
+}
 ?>
