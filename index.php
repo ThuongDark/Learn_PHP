1b6c94013ee9583123bab6f921edb4d4890cadd8
ThuongDark <leoskillvip@gmail.com>
2023-03-08 11:53:43 +0700

    Bài 7.2: Vòng lặp foreach() array

--- index.php
+++ index.php
@@ -1,8 +1,44 @@
 <?php
-/*for (init counter; test counter; increment counter) {
-  code to be executed for each iteration;
-} */
-for ($number = 0; $number <= 100; $number+=10) {
-    echo "Giá trị của number sẽ tăng: " . $number . "</br>";
+//Tìm hiểu sơ về array 
+//Khai báo mảng dãy số
+$arrayNumber = array(1, 2, 3, 4, 5);
+//Khai báo mảng chuỗi
+$arrayString = array('Apple', 'Banana', 'Orange');
+//Khai báo mảng có đánh số index
+$arrayIndex = array(1 => 'Apple', 2 => 'Banna', 3 => 'Orange');
+//Khai báo mảng có key như user
+$arrayIndexUser = array('name' => 'ThuongDark', 'age' => 22, 'job' => 'Đang thất nghiệp');
+//==========================================================
+//echo $arrayNumber;// vì nó là array nên sử dụng print_r hoặc var_dump
+var_dump($arrayNumber); //xem chuỗi trực quan
+echo "</br>";
+echo $arrayString[2];
+echo "</br>";
+echo $arrayIndexUser['name']; //ThuongDark
+echo "</br>" . "============" . "</br>";
+//
+
+/*
+foreach ($array as $value) {
+code to be executed;
 }
+*/
+foreach ($arrayString as $value) {
+    echo "$value <br>";
+}
+echo "</br>" . "============" . "</br>";
+
+/*
+Output: 
+Apple
+Banana
+Orange*/
+foreach ($arrayIndexUser as $x => $value) {
+    echo "$x: $value <br>";
+}
+/*
+name: ThuongDark
+age: 22
+job: Đang thất nghiệp
+*/
 ?>
