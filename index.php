d4383c83252f63e985c16c35d4fa0854a28521c6
ThuongDark <leoskillvip@gmail.com>
2023-03-13 15:50:36 +0700

    Bài 9: Array hàm sort() và rsort()

--- index.php
+++ index.php
@@ -1,11 +1,43 @@
-<?php 
-function familyName($name){
-echo "$name".'<br>';
-}
-familyName('ThuongDark');//ThuongDark
-function number($a,$b)
+<?php
+
+/*********************Array_string ascending order************* */
+$array_strings = array('c','b','a','d');
+sort($array_strings);
+foreach($array_strings as $array_string )
 {
-return $a+$b;
+echo $array_string;//abcd
 }
-echo number(4,40)//44
+echo "</br>"."============"."</br>";
+
+
+/*********************Array_number ascending order************* */
+$array_numbers = array(5,3,2,1,4);
+sort($array_numbers);
+foreach($array_numbers as $array_number )
+{
+echo $array_number;//12345
+}
+echo "</br>"."============"."</br>";
+
+
+/*********************Array_string descending order************* */
+$array_strings = array('c','b','a','d');
+rsort($array_strings);
+foreach($array_strings as $array_string )
+{
+echo $array_string;//abcd
+}
+echo "</br>"."============"."</br>";
+
+
+
+/*********************Array_number descending order************* */
+$array_numbers = array(5,3,2,1,4);
+rsort($array_numbers);
+foreach($array_numbers as $array_number )
+{
+echo $array_number;//12345
+}
+echo "</br>"."============"."</br>";
+
 ?>
